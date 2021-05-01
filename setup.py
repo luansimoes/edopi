0dcd8566af13084cae80714618ecd024a8ce2f52
luansimoes <32573762+luansimoes@users.noreply.github.com>
2021-05-01 01:55:43 -0300

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(name='edopi',
+    version='0.0.1',
+    url='https://github.com/luansimoes/edopi',
+    license='MIT License',
+    author='Flavia Elias e Luan Simões',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='luansimoes@id.uff.br',
+    keywords='Pacote',
+    description='Pacote PyPI para musica microtonal',
+    packages=['edopi'],
+    install_requires=['numpy','matplotlib'],)
