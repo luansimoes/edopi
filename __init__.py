8ae90968ae9f1cb73a7619ed05a43600d80d943a
Luan Simões <luansimoes@id.uff.br>
2021-05-01 02:03:59 -0300

    remove old files

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from domain import TonalSystem, Scale, GCycle, BalzanoDiagram, TonalSystemElement
--- domain/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tonal_system import TonalSystem
-from .tonal_system_element import TonalSystemElement
-from .scale import Scale
-from .gcycle import GCycle
-from .balzano_diagram import BalzanoDiagram
-from .utils import transpose_to_octave
