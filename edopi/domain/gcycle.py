728eb5bab6d36bef676074734eeaa5c16a14972a
Luan Simões <luansimoes@id.uff.br>
2021-05-27 17:17:17 -0300

    Add attribute system_size on class GCycle

--- edopi/domain/gcycle.py
+++ edopi/domain/gcycle.py
@@ -9,7 +9,7 @@ class GCycle:
     """
     Instantiate a G-Cycle of the Tonal System.
     A G-Cycle is a generalized Cycle of Fifths - which is isomorph to the Group.
-    There's no need to interact directly with this class.
+    In most cases, there's no need to interact directly with this class.
     
     :param generator: The generator of the Cycle.
     :type g: TonalSystemElement
@@ -19,11 +19,12 @@ class GCycle:
         assert generator.is_generator(), 'GCycle must be initialized with a element that is a generator of the given system'
         self.generator = generator
         self.elements = self.generate_cycle()
+        self.system_size = self.generator.module
         #self.scale_sizes = sorted([self.generator.inverse().pitch_class, self.generator.inverse().symmetrical().pitch_class])
 
     def generate_cycle(self):
-        elements = [TonalSystemElement(0, self.generator.module)]
-        for _ in range(self.generator.module - 1):
+        elements = [TonalSystemElement(0, self.system_size)]
+        for _ in range(self.system_size - 1):
             elements.append(elements[-1] + self.generator)
         return elements
 
@@ -32,7 +33,7 @@ class GCycle:
     def diatonic_scale(self, tonic: int):
         length = self.generator.inverse().pitch_class
 
-        start_element = TonalSystemElement(-self.generator.pitch_class, self.generator.module)
+        start_element = TonalSystemElement(-self.generator.pitch_class, self.system_size)
         
         sc_elements = [start_element]
         for _ in range(length-1):
@@ -42,17 +43,17 @@ class GCycle:
 
         struct = tuple((sc_elements[i]-sc_elements[i-1]).pitch_class for i in range(1, len(sc_elements)))
 
-        return DiatonicScale(self.generator.module, struct, self.generator, tonic=tonic, name="Diatonic Scale")
+        return DiatonicScale(self.system_size, struct, self.generator, tonic=tonic, name="Diatonic Scale")
 
     # TODO: return integer if elem is an integer
     def next(self, elem: Union[TonalSystemElement, int], steps: int):
-        real_elem = TonalSystemElement(elem, self.generator.module) if isinstance(elem, int) else elem
+        real_elem = TonalSystemElement(elem, self.system_size) if isinstance(elem, int) else elem
         next_index = (self.elements.index(real_elem) + steps) % len(self.elements)
         return copy.deepcopy(self.elements[next_index])
 
     def show(self):
         r = 10
-        angle = 2 * np.pi / self.generator.module
+        angle = 2 * np.pi / self.system_size
         x = [self.elements[0].pitch_class]
         y = [r]
 
@@ -60,13 +61,13 @@ class GCycle:
         cycle = plt.Circle((0, 0), r, fill=False)
         axes.add_artist(cycle)
 
-        for i in range(1, self.generator.module + 1):
+        for i in range(1, self.system_size + 1):
             x.append(r * np.sin(i * angle))
             y.append(r * np.cos(i * angle))
 
         #plt.plot(x, y, markersize=20)
         plt.plot(x, y, 'wo', markersize=20)
-        for i in range(self.generator.module):
+        for i in range(self.system_size):
             plt.text(x[i], y[i],self.elements[i], ha='center', va='center')
         plt.axis('equal')
         plt.axis('off')
